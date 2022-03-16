76547b4cd2aabdbe21cef4b5ad5c9a4a12ec6a3d
sy.park <sy.park@powderroom.co.kr>
2022-03-16 10:36:24 +0900

    .

---
+++ src/test/java/io/simpolor/mockapplication/MockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.simpolor.mockapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
